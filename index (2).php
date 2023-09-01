2e77b73612a28729ebd709beb1a774a7b98635a3
Shahd Elsayed <105386866+ShahdMady@users.noreply.github.com>
2023-09-01 22:20:52 +0300

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <?php include"nav.php";  ?>
+</head>
+<body>
+<div class="content-container">
+        <h1>Welcome to BA Sports</h1>
+        <p>Our team of knowledgeable and friendly staff is here to help you find the perfect needs for you. We pride ourselves on providing exceptional customer service, and we're always here to answer any questions you may have.</p>
+        <button>Contact Us</button>
+</div>
+<div class="image-container">
+        <div class="image-card">
+            <img src="assets/images/1.jpg" alt="Image 1">
+            <h2>Golf Training</h2>
+        </div>
+        <div class="image-card">
+            <img src="assets/images/2.jpg" alt="Image 2">
+            <h2>Best Ground</h2>
+        </div>
+        <div class="image-card">
+            <img src="assets/images/3.jpg" alt="Image 3">
+            <h2>Best Team</h2>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Navigation Bar</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <img src="assets/images/golf.png" alt="Logo">
+            <span>BA SPORTS</span>
+        </div>
+        <div class="navbar-right">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
