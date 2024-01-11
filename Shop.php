0320a480dbca4965a6cde84949ae2c0e49f93db5
seifnasser1 <Seifnasser2001@gmail.com>
2024-01-11 22:22:18 +0200

    Updates in the shop and added some images

--- Shop.php
+++ Shop.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-
     <title>Shop</title>
     <link rel="stylesheet" href="style.css">
-    <?php include"nav.php";  ?>
-</head>
-<style>
+    <?php include "nav.php"; ?>
+    <style>
         body {
             text-align: center;
         }
@@ -14,9 +12,75 @@
         h1 {
             text-align: center;
         }
-</style>
+
+        .product-container {
+            display: inline-block;
+            margin: 10px;
+            width: 30%; /* Adjust as needed */
+            text-align: left;
+        }
+
+        .w3-card-4 img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
 <body>
     <h1>Here we'll have Spark Shop</h1>
-</body>
 
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Shoes.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Asics Shoe</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Bag.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Tecnifibre Bag</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Racket.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Tecnifibre Racket</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Balls.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Dunlop Balls</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Glasses.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Tecnifibre Glasses</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-container">
+        <div class="w3-card-4">
+            <img src="img/Equipment/Spark.jpeg" alt="Alps">
+            <div class="w3-container w3-center">
+                <p>Spark</p>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
