524f3dcb7a836298940dda6faf48bbe1d39fbcda
seifnasser1 <Seifnasser2001@gmail.com>
2024-01-11 18:32:04 +0200

    Edit in shop page

--- Shop.php
+++ Shop.php
@@ -6,5 +6,17 @@
     <link rel="stylesheet" href="style.css">
     <?php include"nav.php";  ?>
 </head>
+<style>
+        body {
+            text-align: center;
+        }
+
+        h1 {
+            text-align: center;
+        }
+</style>
+<body>
+    <h1>Here we'll have Spark Shop</h1>
+</body>
 
 </html>
